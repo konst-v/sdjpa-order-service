69f17bc50a36b9cf99bdca0785914bef9bb25c97
konst-v <konst.vv@gmail.com>
2022-05-21 13:16:27 +0300

    Added Product entity

---
+++ src/main/java/guru/springframework/orderservice/domain/Product.java
@@ -0,0 +1,50 @@
+package guru.springframework.orderservice.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Entity
+public class Product extends BaseEntity {
+
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    private ProductStatus productStatus;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ProductStatus getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(ProductStatus productStatus) {
+        this.productStatus = productStatus;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        Product product = (Product) o;
+
+        if (description != null ? !description.equals(product.description) : product.description != null) return false;
+        return productStatus == product.productStatus;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (productStatus != null ? productStatus.hashCode() : 0);
+        return result;
+    }
+}
---
+++ src/test/java/guru/springframework/orderservice/repositories/ProductRepositoryTest.java
@@ -0,0 +1,40 @@
+package guru.springframework.orderservice.repositories;
+
+import guru.springframework.orderservice.domain.Product;
+import guru.springframework.orderservice.domain.ProductStatus;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class ProductRepositoryTest {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    public void saveProduct() {
+        Product product = new Product();
+        product.setDescription("descr");
+        product.setProductStatus(ProductStatus.NEW);
+
+        Product savedProduct = productRepository.save(product);
+
+        assertNotNull(savedProduct);
+        assertNotNull(savedProduct.getId());
+
+        Product fetchedProduct = productRepository.getById(savedProduct.getId());
+
+        assertNotNull(fetchedProduct);
+        assertNotNull(fetchedProduct.getId());
+        assertEquals(product.getDescription(), fetchedProduct.getDescription());
+        assertEquals(product.getProductStatus(), fetchedProduct.getProductStatus());
+    }
+}
